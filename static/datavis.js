0eccb1b197338d3a3358501a132d1e937eec23a7
ryankashi <ryankashi@berkeley.edu>
2015-12-06 18:07:50 -0800

    Made DV page stop auto refreshing to the top of the page. Made rendering charts much smoother

--- static/datavis.js
+++ static/datavis.js
@@ -5,6 +5,8 @@ var apiUrl = '';
 var chartHolder;
 var userTemplate;
 var currentUsers = [];
+var oldUsers = [];
+var chartDict = {};
 
 	var makeGetRequest = function(url, onSuccess, onFailure) {
 		$.ajax({
@@ -29,14 +31,40 @@ var currentUsers = [];
 		});
 	};
 	
-	var render_users = function(data,id){
+	var update_user = function(data,id){
+		console.log('updating user ' + id);
+		var chart = chartDict[id];
+		chart.options.data = [];
+		var caches = {};
         
+        var i = 0;
+        for(i = 0; i < data.length; i++){
+            var cache_data_for_user = data[i]['data'];
+            var series = {
+                type: "column",
+                name: cache_data_for_user['full_address'],
+                showInLegend: true
+            };
+            
+            chart.options.data.push(series);
+            
+            series.dataPoints = [
+                { label: cache_data_for_user['full_address'], y: cache_data_for_user['number_of_chunks'] }
+                ];   
+            
+        };
+        chart.render();
+		
+	};
+
+	var render_user = function(data,id){
         var newElem = $(userTemplate);
         newElem.removeAttr('id');
-        newElem.attr('id','test'+id);
+        newElem.attr('id', id);
         chartHolder.append(newElem);
         
-        var chart = new CanvasJS.Chart('test'+id);
+        var chart = new CanvasJS.Chart(id);
+		chartDict[id] = chart;
         chart.options.axisY = { prefix: "Chunks Sent "};
         chart.options.data = [];
         var caches = {};
@@ -58,40 +86,58 @@ var currentUsers = [];
             
         };
         chart.render();
-        
-        
     };
-	var start_window = function() {
-		
+	var delete_user = function(id){
+			delete chartDict[id];
+			$('#' + id).remove();
+			
+		};
+
+	var start_window = function() {	
 		var onSuccess = function(data){
 			//Return dictionary of {professor: prof_name, rating_1: value, rating_2: value, etc}
 			console.log(data)
-            chartHolder.html('');
+            //chartHolder.html('');
             var i = 0;
-            for(i = 0; i < data.length; i++){
-            render_users(data[i],i);
-            };    
-		};
+			oldUsers = currentUsers;
+			currentUsers = [];
 			
+			//For each of the current users, either update their graph or render a new graph
+			for(i = 0; i < data.length; i++){
+				currentUsers.push(data[i][0]['data']['user_name']);
+			};
+			
+			for(i = 0; i < data.length; i++){
+				var old_user = oldUsers[i];
+				if((currentUsers.indexOf(old_user) == -1) && (old_user != null)){
+					delete_user(old_user);	
+				};
+			};
+			
+            for(i = 0; i < data.length; i++){
+				var u_name = currentUsers[i];
+				if(oldUsers.indexOf(u_name) == -1){
+            		render_user(data[i],u_name);
+				}else{
+					update_user(data[i],u_name);	
+				};
+            };    
+		};	
 		var onFailure = function(data){
 		//console.error('could not retreive overall ratings');
             console.error(data);
             console.log('there was an error');
 		};
-		
 		makeGetRequest('/req/GET_CACHE_DATA', onSuccess, onFailure);
 		
 		window.setInterval(function(){
-		//series1.dataPoints[0].y = h.getSeconds();
-		//chart.render();	
-			
 		makeGetRequest('/req/GET_CACHE_DATA', onSuccess, onFailure);	
 	}, 10000);	
 	};
 
 	var start = function() {
     	chartHolder = $('#allChartHolder');
-    	//chartHolder = $('body');
+    	chartHolder = $('body');
     	userTemplate = $('#user-template')[0].outerHTML;
     	chartHolder.html('');
 		
