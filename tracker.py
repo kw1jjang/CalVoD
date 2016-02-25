7ddf4aa900b11316c7924f463cc7a13457dc25a9
Jiayuan Chen <chenjiayuancz@gmail.com>
2016-02-24 19:26:25 -0800

    added templates and style for login and registration

--- tracker.py
+++ tracker.py
@@ -1,6 +1,7 @@
 import ast
 import sys
 import web
+from web import form
 import db_manager
 import helper #now importing the helper. The helper is storing the ip address of the tracker. we set the value of this
 from time import gmtime, strftime
@@ -12,6 +13,8 @@ urls = (
     '/', 'overview',
     '/test', 'test',
     '/req/(.*)', 'request',
+    '/registration', 'registration',
+    '/login', 'login',
 )
 #import site
 #site.getsitepackages() to find where your site packages are stored
@@ -356,8 +359,35 @@ class request:
                 user_data = dv.rearrange_data_for_caches(user_data)
                 return json.dumps(user_data)
                 #currently reading from file. must later have post request to store data into db
-                
 
+class registration:
+    def GET(self):
+        return render.registration()
+
+    def POST(self):
+        return web.input()
+
+myform = form.Form(
+    form.Textbox('username'),
+    form.Password('password'),
+    form.Button('Login'),
+)
+
+class login: 
+    def GET(self): 
+        form = myform()
+        # make sure you create a copy of the form by calling it (line above)
+        # Otherwise changes will appear globally
+        return render.formtest(form)
+
+    def POST(self): 
+        form = myform() 
+        if not form.validates(): 
+            return render.formtest(form)
+        else:
+            # form.d.boe and form['boe'].value are equivalent ways of
+            # extracting the validated arguments from the form.
+            return "Grrreat success! boe: %s, bax: %s" % (form.d.boe, form['bax'].value)
 
 if __name__ == "__main__":
     print(len(sys.argv))
