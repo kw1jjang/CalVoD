4ce0ce20dc948d0497522372906623917770fde4
Jiayuan Chen <chenjiayuancz@gmail.com>
2016-03-25 20:25:05 -0700

    added back the original 2 shell scripts for caches

---
+++ populate_caches_public.sh
@@ -0,0 +1,27 @@
+./kill_caches.sh
+
+num_of_caches=5
+echo "Initiating cache, pleae enter the number of caches (default 5):"
+read option
+if [ ! -z "$option" ] ; then
+    num_of_caches=$option
+fi
+
+mkdir -p caches
+rm -rf caches/*
+cd "caches"
+for (( i = 1; i <= num_of_caches; i++ ))
+do
+    echo "Initiating cache with id # $i ..."
+    if [ ! -d "cache"$i ]; then
+        mkdir "cache"$i
+    fi
+    cd "cache"$i
+    rm -rf video*
+    #python ../../cache.py $i > ../../log/cache_$i.txt &
+    python ../../cache.py $i public > /dev/null &
+
+    cd ..
+    sleep .1
+done
+cd ".."
