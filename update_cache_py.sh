5624e4fe0948d6798eea3ddbd07ddc0411205891
kw1jjang <kw1jjang@gmail.com>
2016-06-02 15:00:16 +0900

    Update update_cache_py.sh

--- update_cache_py.sh
+++ update_cache_py.sh
@@ -1,13 +1,13 @@
 echo "updating cache.py on C2 - C6..."
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/cache.py ec2-user@52.7.236.218:~/P2PVoD/cache.py #C2
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/cache.py ec2-user@52.200.196.119:~/P2PVoD/cache.py #C3
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/cache.py ec2-user@52.201.5.148:~/P2PVoD/cache.py #C4
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/cache.py ec2-user@52.201.45.124:~/P2PVoD/cache.py #C5
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/cache.py ec2-user@54.152.185.230:~/P2PVoD/cache.py #C6
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/cache.py ec2-user@52.7.236.218:~/CalVoD/cache.py #C2
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/cache.py ec2-user@52.200.196.119:~/CalVoD/cache.py #C3
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/cache.py ec2-user@52.201.5.148:~/CalVoD/cache.py #C4
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/cache.py ec2-user@52.201.45.124:~/CalVoD/cache.py #C5
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/cache.py ec2-user@54.152.185.230:~/CalVoD/cache.py #C6
 
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/populate_users.sh ec2-user@54.89.22.93:~/P2PVoD/populate_users.sh
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/populate_users.sh ec2-user@54.86.170.149:~/P2PVoD/populate_users.sh
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/populate_users.sh ec2-user@54.84.101.123:~/P2PVoD/populate_users.sh
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/populate_users.sh ec2-user@52.87.246.52:~/P2PVoD/populate_users.sh
-scp -i ~/.ssh/NVirginakey.pem ~/Desktop/P2PVoD/populate_users.sh ec2-user@54.165.253.88:~/P2PVoD/populate_users.sh
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/populate_users.sh ec2-user@54.89.22.93:~/CalVoD/populate_users.sh
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/populate_users.sh ec2-user@54.86.170.149:~/CalVoD/populate_users.sh
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/populate_users.sh ec2-user@54.84.101.123:~/CalVoD/populate_users.sh
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/populate_users.sh ec2-user@52.87.246.52:~/CalVoD/populate_users.sh
+scp -i ~/.ssh/NVirginakey.pem ~/Desktop/CalVoD/populate_users.sh ec2-user@54.165.253.88:~/CalVoD/populate_users.sh
 echo "done!"
